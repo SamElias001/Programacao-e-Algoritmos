dd45a679c5452b1d07ee76de4e2070b6df3efe50
SamElias001 <149211876+SamElias001@users.noreply.github.com>
2025-05-06 19:57:50 -0300

    Add files via upload

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Informe a quantidade de termos da sequência de Fibonacci:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int a = 0, b = 1, c;
+
+Console.WriteLine("Sequência de Fibonacci:");
+if (n >= 1) Console.Write(a + " ");
+if (n >= 2) Console.Write(b + " ");
+
+for (int i = 3; i <= n; i++)
+{
+    c = a + b;
+    Console.Write(c + " ");
+    a = b;
+    b = c; 
+}
+
